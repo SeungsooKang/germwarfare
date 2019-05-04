3e09f5a075c08d2b1765fa9efa75f8aa273e8dbe
John Kang <seun981soo@gmail.com>
2019-05-04 19:33:16 -0400

    mainPane onClick event..done

--- src/App.js
+++ src/App.js
@@ -5,37 +5,10 @@ import MainPane from './MainPane'
 
 class App extends React.Component {
 
-	static playMap = {
-		mapSize: {},
-		data: []
-	}
-
-	createPlayMap = (mapSize) => {
-		App.playMap.mapSize = mapSize;
-		for(var i=0;i<mapSize.numRow;i++){
-			for(var j=0;j<mapSize.numCol;j++){
-				App.playMap.data.push({
-					row: i,
-					col: j,
-					status: 0,
-					bgStatus: 0
-				});
-			}
-		}
-	} 
-
-	constructor(props) {
-		super(props);
-		this.createPlayMap({ numRow: 7, numCol: 7 });
-	}
-
 	componentDidMount() {
-		this.cellElements = document.getElementsByClassName('cell');
-		console.log(this.cellElements);
 	}
 
     render(){
-
         return (
             <Container fluid> 
               <Row>
--- src/MainPane.js
+++ src/MainPane.js
@@ -1,47 +1,73 @@
 import React from 'react'
 import Cell from './Cell'
-import App from './App';
-
-const CreateRow = ({numRow, numCol}) => {
-	var dataRow = (App.playMap.data).filter(data => data.row === numRow);
-    var items = [];
-    for (var i = 0; i < numCol; i++) {
-        items.push(<td key={i}><Cell status={dataRow[i].status} bgStatus={dataRow[i].bgStatus} /></td>);
-    }
-    return <tr>{items}</tr>;
-}
-
-const CreateMap = ({numRow, numCol}) => {
-	var rows = [];
-    for (var i = 0; i < numRow; i++) {
-        rows.push(<CreateRow key={i} numRow={i} numCol={numCol}/>);
-    }
-    return 	<div>
-        		<tbody>{rows}</tbody>
-			</div>
-}
 
 class MainPane extends React.Component {
 
-    mapSizeRow = App.playMap.mapSize.numRow;
-	mapSizeCol = App.playMap.mapSize.numCol;
+	state = {
+		mapSize: { numRow: 7, numCol: 7 },
+		mapData: this.createPlayMap(7, 7)
+	}
+
+	createPlayMap(numRow, numCol) {
+		var mapDataArr = [];
+		for(var i=0;i<numRow;i++){
+			for(var j=0;j<numCol;j++){
+				mapDataArr.push({
+					row: i,
+					col: j,
+					status: 0,
+					bgStatus: 0
+				});
+			}
+		}
+		// initial germs
+		mapDataArr[0].status = 1;
+		mapDataArr[numCol-1].status = 2;
+		mapDataArr[numRow*numCol-numCol].status = 2;
+		mapDataArr[numRow*numCol-1].status = 1;
+
+		return mapDataArr;
+	} 
+
+	createRow = (numRow, numCol) => {
+		var dataRow = (this.state.mapData).filter(data => data.row === numRow);
+		var items = [];
+		for (var i = 0; i < numCol; i++) {
+			items.push(<td key={i}><Cell status={dataRow[i].status} bgStatus={dataRow[i].bgStatus} /></td>);
+		}
+		return <tr key={numRow}>{items}</tr>;
+	}
 	
-    componentDidMount() {
-        
+	createMap = (numRow, numCol) => {
+		var rows = [];
+		for (var i = 0; i < numRow; i++) {
+			// rows.push(<CreateRow key={i} numRow={i} numCol={numCol}/>);
+			rows.push(this.createRow(i, numCol));
+		}
+		return 	<table className='mainpane' onClick={this.onMainPaneClick}>
+					<tbody>{rows}</tbody>
+				</table>
 	}
 
 	onMainPaneClick = (event) => {
 		var mainPanePos = document.getElementsByClassName('mainpane')[0].getBoundingClientRect();
-        var x = Math.floor((event.clientY-mainPanePos.y)/(mainPanePos.height/this.mapSizeRow));
-		var y = Math.floor((event.clientX-mainPanePos.x)/(mainPanePos.width/this.mapSizeCol));
-		App.playMap.data[x*7+y].status = 1;
-		console.log(App.playMap.data);
-		this.setState(this.state);
+        var x = Math.floor((event.clientY-mainPanePos.y)/(mainPanePos.height/this.state.mapSize.numRow));
+		var y = Math.floor((event.clientX-mainPanePos.x)/(mainPanePos.width/this.state.mapSize.numCol));
+		console.log(x);
+		console.log(y);
+		console.log(this.state.mapData);
+		let tempArr = this.state.mapData;
+		tempArr[this.state.mapSize.numCol*x+y].status = 1;
+
+		this.setState({
+			mapData: tempArr
+		});
 	}
  
     render() {
-        return <table className='mainpane' onClick={this.onMainPaneClick}>
-		<CreateMap numRow={this.mapSizeRow} numCol={this.mapSizeCol}/></table>;
+        return <div>
+		{this.createMap(this.state.mapSize.numRow,this.state.mapSize.numCol)}
+		</div>
     }
 }
 
