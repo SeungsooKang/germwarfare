30d0061a497c8026a9c58e5360af66f24447175a
John Kang <seun981soo@gmail.com>
2019-05-10 18:30:35 -0400

    Updates

---
+++ precache-manifest.4db6b16c8ce1bc62eccd06486e8fb297.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c2eeee45e7df5ddaf2b0028de555f3f",
+    "url": "/germwarfare/index.html"
+  },
+  {
+    "revision": "b166b5a8e71c67cf1bc6",
+    "url": "/germwarfare/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "500d8deb973c6af6ebc2",
+    "url": "/germwarfare/static/css/main.a00b02ba.chunk.css"
+  },
+  {
+    "revision": "b166b5a8e71c67cf1bc6",
+    "url": "/germwarfare/static/js/2.dfdf1362.chunk.js"
+  },
+  {
+    "revision": "500d8deb973c6af6ebc2",
+    "url": "/germwarfare/static/js/main.a409d351.chunk.js"
+  },
+  {
+    "revision": "9ef8d68a8568c95c6b64",
+    "url": "/germwarfare/static/js/runtime~main.6e896e74.js"
+  }
+]);
